32c9bc585bb43249cdcaf2c6c6c609dd3b426622
Kurt D <kurtd5105@gmail.com>
2017-03-02 00:25:35 -0800

    Zoom in camera and make background black. Set up quadrant locations, and add a random deviation to a star's position.

--- Assets/Scripts/StarGenerator.cs
+++ Assets/Scripts/StarGenerator.cs
@@ -12,12 +12,20 @@ public class StarGenerator : MonoBehaviour {
 
     private Transform starmap;
     private List<KeyValuePair<int, int>>[] locationsAvailable;
+    private KeyValuePair<int, int>[] quadrantLocations;
 
     void InitializeList() {
+        quadrantLocations = new KeyValuePair<int, int>[quadrants];
+        int i = 0;
+        for (int x = 0; x < 2; x++) {
+            for (int y = 0; y < 2; y++) {
+                quadrantLocations[i] = new KeyValuePair<int, int>(x, y);
+                i++;
+            }
+        }
+
+        // Setup available locations.
         locationsAvailable = new List<KeyValuePair<int, int>>[quadrants];
-
-        // Need to rotate between quadrants for generation.
-
         for (int q = 0; q < quadrants; q++) {
             locationsAvailable[q] = new List<KeyValuePair<int, int>>();
             for (int x = 0; x < rows; x++) {
@@ -29,17 +37,18 @@ public class StarGenerator : MonoBehaviour {
     }
 
     void StarmapSetup() {
-        int spawned = 0;
         starmap = new GameObject("Starmap").transform;
 
+        // TODO: rotate between quadrants for generation.
+        // Generates stars.
         for (int q = 0; q < quadrants; q++) {
             while (locationsAvailable[q].Count > 0) {
+                // TODO: Multiple star types.
+                // The star type to instantiate.
                 GameObject toInstantiate = stars[0];
-
                 GameObject instance = Instantiate(toInstantiate, RandomPosition(q), Quaternion.identity) as GameObject;
 
                 instance.transform.SetParent(starmap);
-                spawned++;
             }
         }
     }
@@ -47,17 +56,21 @@ public class StarGenerator : MonoBehaviour {
     Vector3 RandomPosition(int quadrant) {
         int randomIndex = Random.Range(0, locationsAvailable[quadrant].Count);
         KeyValuePair<int, int> coords = locationsAvailable[quadrant][randomIndex];
-        Vector3 randomPosition = new Vector3(coords.Key, coords.Value, 0f);
+        Vector3 randomPosition = new Vector3(coords.Key + GetRandomDeviation(1), coords.Value + GetRandomDeviation(1), 0f);
 
         // Need to remove coords from other quadrants.
-
-        for (int x = coords.Key - 3; x < coords.Key + 3; x++) {
-            for (int y = coords.Value - 3; y < coords.Value + 3; y++) {
+        locationsAvailable[quadrant].RemoveAt(randomIndex);//(new KeyValuePair<int, int>(x, y));
+        for (int x = coords.Key - 4; x < coords.Key + 4; x++) {
+            for (int y = coords.Value - 4; y < coords.Value + 4; y++) {
                 locationsAvailable[quadrant].Remove(new KeyValuePair<int, int>(x, y));
             }
         }
 
         return randomPosition;
+    }
+
+    float GetRandomDeviation(int max) {
+        return (Random.Range(0, max * 2) - max) * 0.1f;
     }
 
     public void SetupScene() {
