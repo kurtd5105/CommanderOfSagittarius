46bda15c901effd74a687f77eb88e36295a68c34
Kurt D <kurtd5105@gmail.com>
2017-03-16 00:00:35 -0700

    Setup StarProperties turn logic.

--- Assets/Scripts/StarProperties.cs
+++ Assets/Scripts/StarProperties.cs
@@ -17,8 +17,9 @@ public class StarProperties : MonoBehaviour {
 
     public void Generate(StarColor color, bool isHomeworld) {
         if (isHomeworld) {
+            // TODO: These need to vary based on race and difficulty
             population = 50;
-            factories = 0;
+            factories = 30;
             maxPopulation = 100;
             planetType = PlanetTypes.TERRAN;
             planetModifiers.Add(PlanetModifiers.NORMAL);
@@ -32,5 +33,14 @@ public class StarProperties : MonoBehaviour {
         owner = Owners.NONE;
         bases = 0;
         waste = 0;
+    }
+
+    // TODO: Add this to a turn event. Unsubscribe if no owner or no planet. Subscribe if otherwise.
+    public void Turn() {
+        if (planetType != PlanetTypes.NONE && owner != Owners.NONE && population > 0) {
+            // 1M Population costs 20 BC, generate 0.5 BC
+            // 1 Factory costs 10 BC, generates 1 BC and costs 1 BC of waste
+            // Waste costs 0.5 BC to clean up
+        }
     }
 }
