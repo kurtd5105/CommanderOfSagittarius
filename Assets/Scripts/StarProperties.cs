f132196ae88073b1eae9cf95084b98edddbb66bd
Kurt D <kurtd5105@gmail.com>
2017-03-15 22:07:37 -0700

    Add missile bases property. Improve code quality.

--- Assets/Scripts/StarProperties.cs
+++ Assets/Scripts/StarProperties.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StarProperties : MonoBehaviour {
+    // Current owner specifics.
     public float population;
     public float factories;
     public float waste;
+    public float bases;
 
+    // Planet specifics.
     public float maxPopulation;
     public PlanetTypes planetType;
     public List<PlanetModifiers> planetModifiers;
@@ -16,18 +19,18 @@ public class StarProperties : MonoBehaviour {
         if (isHomeworld) {
             population = 50;
             factories = 0;
-            waste = 0;
             maxPopulation = 100;
             planetType = PlanetTypes.TERRAN;
             planetModifiers.Add(PlanetModifiers.NORMAL);
         } else {
             population = 0;
             factories = 0;
-            waste = 0;
             maxPopulation = 0;
             planetType = PlanetTypes.NONE;
             planetModifiers.Add(PlanetModifiers.NORMAL);
         }
         owner = Owners.NONE;
+        bases = 0;
+        waste = 0;
     }
 }
