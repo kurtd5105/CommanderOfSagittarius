24c4dafe523c9b3eb7e7b41917c6e8f079fc67f0
Hsekiu <kpetr7@gmail.com>
2017-03-16 19:36:11 -0700

    Added simple main menu

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void NewGameButton(string NewGameLevel) {
+
+        SceneManager.LoadScene(NewGameLevel);
+	}
+
+    public void ExitGame() {
+
+        Application.Quit();
+    }
+}
