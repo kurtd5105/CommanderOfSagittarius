4be72c20bac9bf236283d93cc804d494d41f17ff
Kurt D <kurtd5105@gmail.com>
2017-07-18 23:51:17 -0700

    Set up population and factories text.

--- Assets/Scripts/InfoPaneManager.cs
+++ Assets/Scripts/InfoPaneManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InfoPaneManager : MonoBehaviour {
     public GameObject starname;
@@ -13,17 +14,35 @@ public class InfoPaneManager : MonoBehaviour {
     public void Init() {
         infoPane = Instantiate(infoPanePrefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity) as GameObject;
 
-        Vector3 position = starname.GetComponent<RectTransform>().position;
-        RectTransform rt = starname.GetComponent<RectTransform>();
-
-        position.y -= rt.rect.height + 5.0f;
-
         population = new NumberDisplayText();
+        factories = new NumberDisplayText();
 
-        population.text = Instantiate(infoPaneText, position, Quaternion.identity) as GameObject;
-        population.text.transform.SetParent(infoPane.transform);
-        population.text.transform.SetAsLastSibling();
+        // Get the RectTransform in order to position the text.
+        RectTransform rt = starname.GetComponent<RectTransform>();
+        TextGenerator textGen = new TextGenerator();
+        TextGenerationSettings generationSettings = starname.GetComponent<Text>().GetGenerationSettings(rt.rect.size);
+        float height = textGen.GetPreferredHeight("Name", generationSettings);
+        
+        population.text = Instantiate(infoPaneText, new Vector3(70.0f, 0.0f, 0.0f), Quaternion.identity, infoPane.transform) as GameObject;
+        factories.text = Instantiate(infoPaneText, new Vector3(70.0f, 0.0f, 0.0f), Quaternion.identity, infoPane.transform) as GameObject;
+
+        // Set correct positioning for the text.
+        Vector3 position = rt.anchoredPosition;
+
+        // Move the text down by the planet name's height plus some extra padding.
+        position.y -= height + 5.0f;
+
+        // Set the population text's position.
+        population.text.GetComponent<RectTransform>().anchoredPosition = position;
+        population.text.GetComponent<RectTransform>().anchoredPosition3D = position;
+
+        position.y -= height + 5.0f;
+
+        // Set the factories text's position.
+        factories.text.GetComponent<RectTransform>().anchoredPosition = position;
+        factories.text.GetComponent<RectTransform>().anchoredPosition3D = position;
 
         population.Init("Population", 0);
+        factories.Init("Factories", 0);
     }
 }
