f7f1c94df6ef78e877cc33e96b871f573961f564
Hsekiu <kpetr7@gmail.com>
2017-08-10 01:14:36 -0700

    Fixed slider code warnings. Fixed UI sorting on InfoCanvas.

--- Assets/Scripts/InfoPaneSlider.cs
+++ Assets/Scripts/InfoPaneSlider.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InfoPaneSlider : MonoBehaviour {
+public class InfoPaneSlider {
     public GameObject SliderLeftObj;
     public GameObject SliderRightObj;
     public GameObject SliderBarObj;
