2106ee40b3dd84b104e34545f582a6eb0ab86c90
Hsekiu <kpetr7@gmail.com>
2017-08-12 19:00:17 -0700

    Finished DropDown interactive functionality.

--- Assets/Scripts/NewGameManager.cs
+++ Assets/Scripts/NewGameManager.cs
@@ -1,8 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NewGameManager : MonoBehaviour {
 
+    public GameObject raceDropObj;
+    public GameObject flagDropObj;
+
+    public Dropdown raceDrop;
+    public Dropdown flagDrop;
+
+    public InputField civInput;
+    public InputField raceInput;
+    public InputField homeInput;
+
+    public Sprite[] flags;
+
+    public void Start() {
+
+        raceDropObj = GameObject.Find("Select Race Drop");
+        flagDropObj = GameObject.Find("Flag Drop");
+
+        raceDrop = raceDropObj.GetComponent<Dropdown>();
+        flagDrop = flagDropObj.GetComponent<Dropdown>();
+
+        raceDrop.ClearOptions();
+        flagDrop.ClearOptions();
+
+        populate();
+    }
+
+    public void populate() {
+        flags = Resources.LoadAll<Sprite>("Flags");
+        Debug.Log(flags.Length);
+
+        foreach(string race in Enum.GetNames(typeof(Races))) {
+            raceDrop.options.Add(new Dropdown.OptionData(race));
+        }
+
+        foreach(Sprite flag in flags) {
+            flagDrop.options.Add(new Dropdown.OptionData(flag));
+        }
+
+        raceDrop.RefreshShownValue();
+        flagDrop.RefreshShownValue();
+    }
 }
