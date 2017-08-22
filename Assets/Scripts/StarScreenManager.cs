2deac70b34eda175cf117878b60f218bbe11da89
Hsekiu <kpetr7@gmail.com>
2017-08-22 15:17:08 -0700

    Added Scroll zoom to starmap.

--- Assets/Scripts/StarScreenManager.cs
+++ Assets/Scripts/StarScreenManager.cs
@@ -4,19 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class StarScreenManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+public class StarScreenManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IScrollHandler {
 
     public GameObject StarmapObj;
+    public GameObject HomeworldObj;
 
-    bool mouseDown;
+    public bool mouseDown;
 
     public Vector2 delta = Vector2.zero;
     public Vector2 lastPos = Vector2.zero;
     public Vector2 currPos = Vector2.zero;
-    public Vector3 temp = Vector3.zero;
 
     void Start() {
         StarmapObj = GameObject.Find("Starmap");
+        HomeworldObj = GameObject.Find("Homeworlds");
+        StarmapObj.transform.localScale = new Vector3(3.0F, 3.0F, 1.0F);
+        HomeworldObj.transform.localScale = StarmapObj.transform.localScale;
+
         mouseDown = false;
     }
 
@@ -27,17 +31,14 @@ public class StarScreenManager : MonoBehaviour, IPointerDownHandler, IPointerUpH
 
             if (lastPos.x != 0 && lastPos.y != 0) {
                 delta = currPos - lastPos;
-            }
-            else {
+            } else {
                 delta = Vector2.zero;
             }
 
-            //Debug.Log("Delta is: " + delta);
             lastPos = currPos;
 
-            temp.x = delta.x/10f;
-            temp.y = delta.y/10f;
-            StarmapObj.transform.position += temp;
+            StarmapObj.transform.position += new Vector3(delta.x / 30.0f, delta.y / 30.0f, 1.0F);
+            HomeworldObj.transform.position = StarmapObj.transform.position;
         }
     }
 
@@ -49,6 +50,21 @@ public class StarScreenManager : MonoBehaviour, IPointerDownHandler, IPointerUpH
         mouseDown = false;
         delta = Vector2.zero;
         lastPos = Vector2.zero;
-        //Debug.Log("Delta is: " + delta);
+    }
+
+    public void OnScroll(PointerEventData eventData)
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        //Scroll Up
+        if (scroll > 0f && (StarmapObj.transform.localScale.x < 3.0F)) {
+            StarmapObj.transform.localScale += new Vector3(0.1F, 0.1F, 0);
+       
+        //Scroll Down
+        } else if (scroll < 0f && (StarmapObj.transform.localScale.x > 2.0F)) {
+            StarmapObj.transform.localScale += new Vector3(-0.1F, -0.1F, 0);
+        }
+
+        HomeworldObj.transform.localScale = StarmapObj.transform.localScale;
     }
 }
