8005f7925e8a077754893d74cfa40e5857bc404a
Hsekiu <kpetr7@gmail.com>
2017-08-22 23:00:09 -0700

    Added preliminary blocking of panning outside starmap bounds. Fixed Z scrolling bug.

--- Assets/Scripts/StarScreenManager.cs
+++ Assets/Scripts/StarScreenManager.cs
@@ -10,10 +10,13 @@ public class StarScreenManager : MonoBehaviour, IPointerDownHandler, IPointerUpH
     public GameObject HomeworldObj;
 
     public bool mouseDown;
+    public bool canMove;
 
     public Vector2 delta = Vector2.zero;
     public Vector2 lastPos = Vector2.zero;
     public Vector2 currPos = Vector2.zero;
+
+    public Vector3 starPos;
 
     void Start() {
         StarmapObj = GameObject.Find("Starmap");
@@ -21,12 +24,14 @@ public class StarScreenManager : MonoBehaviour, IPointerDownHandler, IPointerUpH
         StarmapObj.transform.localScale = new Vector3(3.0F, 3.0F, 1.0F);
         HomeworldObj.transform.localScale = StarmapObj.transform.localScale;
 
+        canMove = false;
         mouseDown = false;
     }
 
     void Update() {
 
         if (mouseDown == true) {
+            starPos = StarmapObj.transform.position;
             currPos = (Vector2)Input.mousePosition;
 
             if (lastPos.x != 0 && lastPos.y != 0) {
@@ -35,10 +40,26 @@ public class StarScreenManager : MonoBehaviour, IPointerDownHandler, IPointerUpH
                 delta = Vector2.zero;
             }
 
+            //Horizontal Pan Blocking
+            if ((starPos.x >= -10 && delta.x > 0) || (starPos.x <= -17 && delta.x < 0)) {
+                canMove = false;
+                //Debug.Log("Horizontal block" + delta.x);
+            }
+            //Vertical Pan Blocking
+            if ((starPos.y <= -20 && delta.y < 0) || (starPos.y >= 0 && delta.y > 0))
+            {
+                canMove = false;
+                //Debug.Log("Vertical block" + delta.y);
+            }
+
             lastPos = currPos;
 
-            StarmapObj.transform.position += new Vector3(delta.x / 30.0f, delta.y / 30.0f, 1.0F);
-            HomeworldObj.transform.position = StarmapObj.transform.position;
+            if (canMove) {
+                StarmapObj.transform.position += new Vector3(delta.x / 30.0f, delta.y / 30.0f, 0.0F);
+                HomeworldObj.transform.position += new Vector3(delta.x / 30.0f, delta.y / 30.0f, 0.0F);
+            }
+
+            canMove = true;
         }
     }
 
@@ -58,11 +79,11 @@ public class StarScreenManager : MonoBehaviour, IPointerDownHandler, IPointerUpH
 
         //Scroll Up
         if (scroll > 0f && (StarmapObj.transform.localScale.x < 3.0F)) {
-            StarmapObj.transform.localScale += new Vector3(0.1F, 0.1F, 0);
+            StarmapObj.transform.localScale += new Vector3(0.1F, 0.1F, 0.0F);
        
         //Scroll Down
         } else if (scroll < 0f && (StarmapObj.transform.localScale.x > 2.0F)) {
-            StarmapObj.transform.localScale += new Vector3(-0.1F, -0.1F, 0);
+            StarmapObj.transform.localScale += new Vector3(-0.1F, -0.1F, 0.0F);
         }
 
         HomeworldObj.transform.localScale = StarmapObj.transform.localScale;
