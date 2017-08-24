d3cae6710bf7127c8bd4cc410b667150b3bc12ce
Hsekiu <kpetr7@gmail.com>
2017-08-23 23:59:12 -0700

    Refactored Starscreen manager code.

--- Assets/Scripts/StarScreenManager.cs
+++ Assets/Scripts/StarScreenManager.cs
@@ -10,7 +10,6 @@ public class StarScreenManager : MonoBehaviour, IPointerDownHandler, IPointerUpH
     public GameObject HomeworldObj;
 
     public bool mouseDown;
-    public bool canMove;
 
     public Vector2 delta = Vector2.zero;
     public Vector2 lastPos = Vector2.zero;
@@ -24,43 +23,12 @@ public class StarScreenManager : MonoBehaviour, IPointerDownHandler, IPointerUpH
         StarmapObj.transform.localScale = new Vector3(3.0F, 3.0F, 1.0F);
         HomeworldObj.transform.localScale = StarmapObj.transform.localScale;
 
-        canMove = false;
         mouseDown = false;
     }
 
     void Update() {
 
-        if (mouseDown == true) {
-            starPos = StarmapObj.transform.position;
-            currPos = (Vector2)Input.mousePosition;
-
-            if (lastPos.x != 0 && lastPos.y != 0) {
-                delta = currPos - lastPos;
-            } else {
-                delta = Vector2.zero;
-            }
-
-            //Horizontal Pan Blocking
-            if ((starPos.x >= -10 && delta.x > 0) || (starPos.x <= -17 && delta.x < 0)) {
-                canMove = false;
-                //Debug.Log("Horizontal block" + delta.x);
-            }
-            //Vertical Pan Blocking
-            if ((starPos.y <= -20 && delta.y < 0) || (starPos.y >= 0 && delta.y > 0))
-            {
-                canMove = false;
-                //Debug.Log("Vertical block" + delta.y);
-            }
-
-            lastPos = currPos;
-
-            if (canMove) {
-                StarmapObj.transform.position += new Vector3(delta.x / 30.0f, delta.y / 30.0f, 0.0F);
-                HomeworldObj.transform.position += new Vector3(delta.x / 30.0f, delta.y / 30.0f, 0.0F);
-            }
-
-            canMove = true;
-        }
+        mousePan();
     }
 
     public void OnPointerDown(PointerEventData eventData) {
@@ -87,5 +55,30 @@ public class StarScreenManager : MonoBehaviour, IPointerDownHandler, IPointerUpH
         }
 
         HomeworldObj.transform.localScale = StarmapObj.transform.localScale;
+    }
+
+    public void mousePan() {
+        if (mouseDown == true) {
+            starPos = StarmapObj.transform.position;
+            currPos = (Vector2)Input.mousePosition;
+
+            if (lastPos.x != 0 && lastPos.y != 0)
+            {
+                delta = currPos - lastPos;
+            } else {
+                delta = Vector2.zero;
+            }
+
+            //Starscreen pan Blocking
+            //Todo: Fix boundaries they are hardcoded and incorrect.
+            if (!((starPos.x >= -10 && delta.x > 0) || (starPos.x <= -17 && delta.x < 0)
+                || (starPos.y <= -20 && delta.y < 0) || (starPos.y >= 0 && delta.y > 0))) {
+                //Not touching boundaries
+                StarmapObj.transform.position += new Vector3(delta.x / 30.0f, delta.y / 30.0f, 0.0F);
+                HomeworldObj.transform.position += new Vector3(delta.x / 30.0f, delta.y / 30.0f, 0.0F);
+            }
+
+            lastPos = currPos;
+        }
     }
 }
