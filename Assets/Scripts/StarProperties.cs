da56d679607b0f5591f5a490a713d14259aa8a15
Hsekiu <kpetr7@gmail.com>
2017-08-30 02:01:50 -0700

    Updated border handling of panning and scrolling.

--- Assets/Scripts/StarProperties.cs
+++ Assets/Scripts/StarProperties.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 
 public class StarProperties {
     public float population;
--- Assets/Scripts/StarScreenManager.cs
+++ Assets/Scripts/StarScreenManager.cs
@@ -18,14 +18,35 @@ public class StarScreenManager : MonoBehaviour, IPointerDownHandler, IPointerUpH
 
     public Vector2 maxStarPositions;
     public Vector2 minStarPositions;
+    public Vector2 oldmax;
+    public Vector2 oldmin;
 
     public Vector3 starPos;
+
+    public Vector3 scale = Vector2.zero;
 
     void Start() {
         StarmapObj = GameObject.Find("Starmap");
         HomeworldObj = GameObject.Find("Homeworlds");
-        StarmapObj.transform.localScale = new Vector3(3.0F, 3.0F, 1.0F);
-        HomeworldObj.transform.localScale = StarmapObj.transform.localScale;
+
+        Debug.Log("Min x is: " + minStarPositions.x);
+        oldmax = maxStarPositions;
+        oldmin = minStarPositions;
+
+        scale = new Vector3(3.0F, 3.0F, 1.0F);
+
+        updateScale(scale.x, scale.y);
+
+        StarmapObj.transform.position = new Vector3(((maxStarPositions.x - minStarPositions.x) / 2) - (maxStarPositions.x / 4), 
+            ((maxStarPositions.y - minStarPositions.y) / 2) - (maxStarPositions.x / 4), 1.0F);
+        HomeworldObj.transform.position = StarmapObj.transform.position;
+
+        try {
+            StarmapObj.transform.localScale = scale;
+            HomeworldObj.transform.localScale = StarmapObj.transform.localScale;
+        } catch (Exception) {
+            Debug.Log("Did not create Game manager from start screen.");
+        }
 
         mouseDown = false;
     }
@@ -47,15 +68,22 @@ public class StarScreenManager : MonoBehaviour, IPointerDownHandler, IPointerUpH
     public void OnScroll(PointerEventData eventData)
     {
         float scroll = Input.GetAxis("Mouse ScrollWheel");
+        float val = 0F;
 
         //Scroll Up
         if (scroll > 0f && (StarmapObj.transform.localScale.x < 3.0F)) {
-            StarmapObj.transform.localScale += new Vector3(0.1F, 0.1F, 0.0F);
-       
-        //Scroll Down
-        } else if (scroll < 0f && (StarmapObj.transform.localScale.x > 2.0F)) {
-            StarmapObj.transform.localScale += new Vector3(-0.1F, -0.1F, 0.0F);
+            val = 0.1F;
         }
+
+        //Scroll Down
+        if (scroll < 0f && (StarmapObj.transform.localScale.x > 2.0F)) {
+            val = -0.1F;
+        }
+
+        StarmapObj.transform.localScale += new Vector3(val, val, 0.0F);
+
+        updateScale(StarmapObj.transform.localScale.x + val, StarmapObj.transform.localScale.y + val);
+        Debug.Log("Scale is: " + StarmapObj.transform.localScale.x + val);
 
         HomeworldObj.transform.localScale = StarmapObj.transform.localScale;
     }
@@ -76,14 +104,36 @@ public class StarScreenManager : MonoBehaviour, IPointerDownHandler, IPointerUpH
             clampPos.x = Mathf.Clamp(starPos.x + (delta.x / 20.0F), maxStarPositions.x, minStarPositions.x);
             clampPos.y = Mathf.Clamp(starPos.y + (delta.y / 20.0F), maxStarPositions.y, minStarPositions.y);
 
-            if ((clampPos.x != minStarPositions.x && clampPos.x != maxStarPositions.x) 
-                && (clampPos.y != minStarPositions.y && clampPos.y != maxStarPositions.y)) {
+            //X panning and boundary check.
+            if ((clampPos.x != minStarPositions.x && clampPos.x != maxStarPositions.x) || 
+                (((StarmapObj.transform.position.x < maxStarPositions.x) && (delta.x > 0)) || 
+                ((StarmapObj.transform.position.x < maxStarPositions.x) && (delta.x > 0) ))) {
 
-                StarmapObj.transform.position += new Vector3(delta.x / 20.0F, delta.y / 20.0F, 0.0F);
-                HomeworldObj.transform.position += new Vector3(delta.x / 20.0F, delta.y / 20.0F, 0.0F);
+                StarmapObj.transform.position += new Vector3(delta.x / 20.0F, 0, 0.0F);
+                HomeworldObj.transform.position += new Vector3(delta.x / 20.0F, 0, 0.0F);
+            }
+
+            //Y panning and boundary check.
+            if ((clampPos.y != minStarPositions.y && clampPos.y != maxStarPositions.y) ||
+                (((StarmapObj.transform.position.y < maxStarPositions.y) && (delta.y > 0)) ||
+                ((StarmapObj.transform.position.y < maxStarPositions.y) && (delta.y > 0)))) {
+                StarmapObj.transform.position += new Vector3(0, delta.y / 20.0F, 0.0F);
+                HomeworldObj.transform.position += new Vector3(0, delta.y / 20.0F, 0.0F);
             }
 
             lastPos = currPos;
         }
+    }
+
+    public void updateScale(float scaleX, float scaleY) {
+        Debug.Log(" ( " + oldmax.x + " * " + scaleX + " ) " + " + " + oldmax.x);
+        Debug.Log(" ( " + oldmin.x + " * " + scaleX + " ) " + " + " + oldmin.x);
+        maxStarPositions.x = (oldmax.x * scaleX) - (oldmax.x);
+        maxStarPositions.y = (oldmax.y * scaleY) - (oldmax.y);
+        minStarPositions.x = (oldmin.x * scaleX) - (oldmin.x);
+        //minStarPositions.y = (minStarPositions.y * scaleY) + StarmapObj.transform.position.y;
+
+        Debug.Log("New max is: " + maxStarPositions);
+        Debug.Log("New min is: " + minStarPositions);
     }
 }
