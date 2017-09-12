c7869b231c3fc3df89af6683ba7da0ea98885380
Hsekiu <kpetr7@gmail.com>
2017-09-11 21:49:52 -0700

    Created Research UI selection skeleton prefab and class.

---
+++ Assets/Scripts/ResearchSelectionUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResearchSelectionUI : MonoBehaviour {
+
+    public Text TitlePanel;
+    public Text DescriptionText;
+
+    public GameObject ResearchPanelObj;
+
+    void Start() {
+        ResearchPanelObj.SetActive(true);
+    }
+
+    void UpdateText(string Title, string Description) {
+        TitlePanel.text = Title;
+        DescriptionText.text = Description;
+    }
+}
