1ef413ac4570d6522ee36c0008371421c1e7a582
Hsekiu <kpetr7@gmail.com>
2017-09-12 23:41:19 -0700

    Changed Research UI prefab and script.

--- Assets/Scripts/ResearchSelectionUI.cs
+++ Assets/Scripts/ResearchSelectionUI.cs
@@ -8,14 +8,50 @@ public class ResearchSelectionUI : MonoBehaviour {
     public Text TitlePanel;
     public Text DescriptionText;
 
+    public GameObject ReserachButton1Obj;
+    public GameObject ReserachButton2Obj;
+    public GameObject ReserachButton3Obj;
+    public GameObject ReserachButton4Obj;
+
+    public Button ResearchButton1;
+    public Button ResearchButton2;
+    public Button ResearchButton3;
+    public Button ResearchButton4;
+
     public GameObject ResearchPanelObj;
 
+    public List<GameObject> buttonsObj;
+
     void Start() {
-        ResearchPanelObj.SetActive(true);
+        ResearchPanelObj.SetActive(false);
+
+        ReserachButton1Obj = GameObject.Find("Technology_1_Button");
+        ReserachButton2Obj = GameObject.Find("Technology_2_Button");
+        ReserachButton3Obj = GameObject.Find("Technology_3_Button");
+        ReserachButton4Obj = GameObject.Find("Technology_4_Button");
+
+        ResearchButton1 = ReserachButton1Obj.GetComponent<Button>();
+        ResearchButton2 = ReserachButton2Obj.GetComponent<Button>();
+        ResearchButton3 = ReserachButton3Obj.GetComponent<Button>();
+        ResearchButton4 = ReserachButton4Obj.GetComponent<Button>();
+
+        buttonsObj.Add(ReserachButton1Obj);
+        buttonsObj.Add(ReserachButton1Obj);
+        buttonsObj.Add(ReserachButton1Obj);
+        buttonsObj.Add(ReserachButton1Obj);
     }
 
-    void UpdateText(string Title, string Description) {
+    void UpdateText(string Title, string Description, string[] Research) {
+        UpdateDimensions(Research.Length);
+
         TitlePanel.text = Title;
         DescriptionText.text = Description;
+    }
+
+    void UpdateDimensions(int number) {
+
+        for(int i = 0; i < number; i++) {
+            buttonsObj[i].SetActive(true);
+        }
     }
 }
